b45b6170167c6d1c4092949f2656ecec2d667704
ikeda <ikd618@gmail.com>
2018-07-25 18:28:02 +0900

    Add Artboard & text

--- src/my-command.js
+++ src/my-command.js
@@ -1,3 +1,17 @@
-export default function(context) {
-  context.document.showMessage("It's alive 🙌")
+import React from 'react';
+import { render, Text, Artboard } from 'react-sketchapp';
+
+export default (context) => {
+  render(
+    <Artboard
+      name='Sample Artboard'
+      style={{
+        width: 375,
+        height: 667,
+      }}
+    >
+      <Text>Hello world! in Artboard</Text>
+    </Artboard>,
+    context.document.currentPage()
+  );
 }
